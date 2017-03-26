8d10860eaa8110070605da48d2d045f2a5ecfc45
Torin J. Carey <torin.j.carey@gmail.com>
2017-03-26 23:16:16 +0100

    Removed 'using namespace std' and changed optimisation Optimisation is now set at -02

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CC=g++
 BIN=./bin
 SRC=./src
 
-CFLAGS=-O3 -I$(SRC) -g
+CFLAGS=-O2 -I$(SRC) -g
 
 DEPS=$(SRC)/matrix.hpp
 
--- src/prettyprint.cpp
+++ src/prettyprint.cpp
@@ -6,12 +6,11 @@
 
 #include "matrix.hpp"
 
-using namespace std;
 using namespace matrix;
 
-string Matrix::pretty() const {
-	ostringstream osstr, main;
-	vector<string> strs;
+std::string Matrix::pretty() const {
+	std::ostringstream osstr, main;
+	std::vector<std::string> strs;
 	uint mwidth = 0;
 	for (uint j = 0; j < n_; ++j) {
 		for (uint i = 0; i < m_; ++i) {
@@ -19,29 +18,29 @@ string Matrix::pretty() const {
 			if (EQUAL(term,0))
 				term = 0;
 			osstr << term;
-			string str = osstr.str();
-			osstr.str(string());
+			std::string str = osstr.str();
+			osstr.str(std::string());
 			strs.push_back(str);
 			uint nl = str.size();
 			mwidth = nl > mwidth ? nl : mwidth;
 		}
 	}
 	uint midwidth = (mwidth * n_) + ((n_ + 1) << 1);
-	main << "\u250c\u2500" << setw(midwidth - 2) << "" << "\u2500\u2510";
+	main << "\u250c\u2500" << std::setw(midwidth - 2) << "" << "\u2500\u2510";
 	for (uint i = 0; i < m_; i++) {
-		main << endl;
+		main << std::endl;
 		main << "\u2502";
 		for (uint j = 0; j < n_; j++) {
-			string& str = strs[i + (j * m_)];
+			std::string& str = strs[i + (j * m_)];
 			int wlen = (str.size() + mwidth + 1) >> 1;
 			main << "  ";
-			main << setw(wlen);
+			main << std::setw(wlen);
 			main << str;
-			main << setw(mwidth - wlen) << "";
+			main << std::setw(mwidth - wlen) << "";
 		}
 		main << "  \u2502";
 
 	}
-	main << endl << "\u2514\u2500" << setw(midwidth - 2) << "" << "\u2500\u2518";
+	main << std::endl << "\u2514\u2500" << std::setw(midwidth - 2) << "" << "\u2500\u2518";
 	return main.str();
 }
